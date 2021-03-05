ca942c39233e3e08856a005a1911e30fed22e51a
danil syah arihardjo <danilsyaharihardjo@gmail.com>
2021-03-05 08:38:54 +0700

    endpoint API Delete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -42,24 +42,97 @@ def serialize_book_detail(book):
         'preview': book.preview,
     }
 
-
+# view all data book
 @app.route('/books', methods=['GET'])
 def book_list():
     books = Books.query.all()
+
+    if len(books) == 0 :
+        return response_success({'result': 'data is empty'})
+
     books = [serialize_book_detail(book) for book in books]
     return response_success({'result': books})
 
 
+# view detail book
 @app.route('/books/<book_id>', methods = ['GET'])
 def book_detail(book_id):
     book = Books.query.filter_by(id=book_id)
     book = book.first()
 
     if book == None:
-        return 
+        return response_failed({
+            'message' : f'Book #{book_id} does not exist'
+        })
 
     book = serialize_book_detail(book)
     return response_success({'result': book})
+
+
+# create data book
+@app.route('/books', methods=['POST'])
+def book_add():
+    req_data = request.get_json()
+
+    book_title = req_data.get('title')
+    book_author = req_data.get('author')
+    book_preview = req_data.get('preview')
+
+    book = Books(
+        title = book_title,
+        author = book_author,
+        preview = book_preview
+    )
+
+    try:
+        db.session.add(book)
+        db.session.commit()
+        return response_success()
+    except Exception as e:
+        return response_failed()
+
+
+# update data book
+@app.route('/books/<book_id>', methods = ['PUT'])
+def book_update(book_id):
+    book = Books.query.filter_by(id=book_id)
+    book = book.first()
+
+    if book == None:
+        return response_failed({
+            'message':f'Book #{book_id} does not exist'
+        })
+
+    req_data = request.get_json()
+
+    try:
+        book.title = req_data.get('title')
+        book.author = req_data.get('author')
+        book.preview = req_data.get('preview')
+        db.session.commit()
+        return response_success()
+    except Exception as e:
+        return response_failed()
+
+
+# delete data book
+@app.route('/books/<book_id>', methods=['DELETE'])
+def book_delete(book_id):
+    book = Books.query.filter_by(id=book_id)
+    book = book.first()
+
+    if book == None:
+        return response_failed({
+            'message': f'Book #{book_id} does not exists'
+        })
+
+    try:
+        db.session.delete(book)
+        db.session.commit()
+        return response_success()
+    except Exception as e:
+        return response_failed()
+
 
 
 if __name__ == '__main__':
